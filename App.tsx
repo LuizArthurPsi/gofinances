fea9d1e723975fb326750406028519abc9444fb8
Luiz Arthur <luizarthurvm@gmail.com>
2022-06-26 15:11:21 -0300

    Add estilos globais
    Extensão de tipagem do styled components para o theme criado

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './src/global/theme';
 import Welcome from './src/Components/Dashboard';
 
 export default function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Welcome title='Hello World'/>
+    </ThemeProvider>
   );
 }
